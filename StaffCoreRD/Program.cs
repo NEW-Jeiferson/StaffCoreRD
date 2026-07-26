ef7154462155fa6769fd6d78fbc60e621aafe3c0
David <jeffersonpaez5@gmail.com>
2026-07-26 15:19:46 -0400

    Commit 4: Program.cs (Identity, cookie, orden de middleware, roles automáticos) + migración inicial

--- StaffCoreRD/Program.cs
+++ StaffCoreRD/Program.cs
@@ -1,15 +1,42 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StaffCoreRD.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. DbContext
+builder.Services.AddDbContext<StaffDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StaffCore")));
+
+// 2. Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = true;
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+})
+.AddEntityFrameworkStores<StaffDbContext>()
+.AddDefaultTokenProviders();
+
+// Cookie de autenticación
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/Login";
+});
+
+// 3. MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +45,25 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // SIEMPRE antes de Authorization - error fatal si se invierte
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Crear roles automáticamente al iniciar
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roles = { "Administrador", "RRHH", "Viewer" };
+    foreach (var rol in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(rol))
+        {
+            await roleManager.CreateAsync(new IdentityRole(rol));
+        }
+    }
+}
 
 app.Run();
